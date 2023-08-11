8c3234e70f83cdb8093d1c5886400172e64a0ae8
AlbertoEGM <blackryu2005@gmail.com>
2023-08-10 22:45:09 -0600

    Primer codigo express con post

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res)=>{
     res.send('Hola clase');
 });
 
+app.post('/crearUsuarios/:id', (req, res)=>{
+    const userID = parseInt(req.params.id);
+    res.send(`Felicidades creo el User nuevo con el id: ${userID}...`)
+})
+
 app.listen(PORT,()=>{
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 })
