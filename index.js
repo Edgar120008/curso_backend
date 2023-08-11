a1855bc149386c614a96d0a1a8b2ba3d9cf10ad0
Dario <darioalvaradodiaz987@gmail.com>
2023-08-10 22:27:51 -0600

    Implementando express

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('¡Hola, mundo!');
-    } else if (req.url === '/usuarios') {
-      // Manejar GET para /usuarios
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      const usuarios = [{ id: 1, nombre: 'Usuario 1' }, { id: 2, nombre: 'Usuario 2' }];
-      res.end(JSON.stringify(usuarios));
-    }
-  } else if (req.method === 'POST') {
-    if (req.url === '/usuarios') {
-      // Manejar POST para /usuarios
-      // ... parsear datos y guardar en la base de datos, por ejemplo ...
-      res.writeHead(201, { 'Content-Type': 'text/plain' });
-      res.end('Usuario creado con éxito');
-    }
-  } else if (req.method === 'PUT') {
-    if (req.url === '/usuarios/1') {
-      // Manejar PUT para /usuarios/1
-      // ... actualizar datos del usuario con ID 1 ...
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Usuario actualizado con éxito');
-    }
-  } else if (req.method === 'DELETE') {
-    if (req.url === '/usuarios/1') {
-      // Manejar DELETE para /usuarios/1
-      // ... eliminar usuario con ID 1 ...
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Usuario eliminado con éxito');
-    }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Ruta no encontrada');
-  }
-});
+const express = require('express');
+const app = express();
 
 const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+
+app.get('/',(req,res)=>{
+    res.send("Hola clase");
 });
+app.listen(PORT,()=>{
+    console.log("Servidor escuchando en el puerto");
+})
