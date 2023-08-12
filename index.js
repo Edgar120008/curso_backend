7bbd5a9fe28c959d0be1d25ace04b9cd1678b26a
AlbertoEGM <blackryu2005@gmail.com>
2023-08-12 17:51:22 -0600

    Tarea express

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ app.post('/crearUsuarios/:id', (req, res)=>{
     const userID = parseInt(req.params.id);
     res.send(`Felicidades creo el User nuevo con el id: ${userID}...`)
 })
+app.put('/actualizarUser/:id', (req, res)=>{
+    const User=parseInt(req.params.id);
+    res.send(`El usuario con el id: ${User} fue modificado`)
+})
+app.delete('/EliminarUser/:id', (req, res)=>{
+    const Borrar=parseInt(req.params.id);
+    res.send(`El usario ${Borrar} fue eliminado`)
+})
 
 app.listen(PORT,()=>{
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 })
+
+
+
+// get post put y delete
