9fc21ae7023a66643a91dfd4884a9091651718f5
AlbertoEGM <blackryu2005@gmail.com>
2023-08-17 23:40:22 -0600

    api

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// middleware para parserar JSON en las solicitudes
 app.use(express.json());
 
-// midleware perzonalizado para registrar solicitudes
-app.use((req, res, next)=>{
-  console.log(`Solicitud recibida en ${new Date()}`)
-  next();
+let tareas =[
+  {id:1, titulo:'Hacer Tarea'},
+  {id:2, titulo:'Estuduar Express'}
+];
+
+// esta ruta manda como respuesta todas las tareas
+app.get('/tareas', (req, res) =>{
+  res.json(tareas);
 })
 
-// middleware perzonalizado para autnticacion basica
-const auth = (req, res, next) =>{
-  const palabra = req.headers.authorization;
+// esta ruta manda como respuesta solo la tarea que se indique en el parametro
+app.get('/tarea/:id', (req, res)=>{
+  const id = parseInt(req.params.id);
+  const tarea = tareas.find(t => t.id === id);
 
-  if(palabra==='llave'){
-    next(); //continua si el valor de la palabra es la correcta
-  }else{
-    res.status(401).json({error:'el valor de la palabra no es correcto'});
+  if(isNaN(id)){
+    res.status(400).json({mensaje: 'por favor ingresa un numero como parametro'})
   }
-};
+  if(tarea){
+    res.json(tarea)
+  }else{
+    res.status(404).json({mensaje: `no existe esa tarea con el id ${id}`})
+  }
+})
 
-app.get('/ruta/palabra', auth, (req, res)=>{
-  res.json({mensaje: 'se ingreso con exito a la ruta'});
-});
+// esta ruta crea una nueva tarea
+app.post('/nuevaTarea', (req, res)=>{
+  const nuevaTarea = req.body;
+  nuevaTarea.id = tareas.length + 1;
+  tareas.push(nuevaTarea);
+  res.status(201).json(nuevaTarea);
+})
 
-app.listen(PORT, ()=>{
+// resta ruta actualiza las tareas
+app.put('/actualizarTarea/:id', (req, res)=>{
+  const id = parseInt (req.params.id);
+  const actualizacionTarea =req.body;
+  const index = tareas.findIndex(t => t.id === id);
+
+  if(index !== -1){
+    tareas[index] ={ ...tareas[index], ...actualizacionTarea};
+    res.json(tareas[index]);
+  }else{
+    res.status(404).json({mensaje:'Tarea no encontrada'})
+  }
+})
+
+// ruta para eliminar tareas
+app.delete('/borrarTareas/:id', (req, res)=>{
+  const id = parseInt (req.params.id);
+  const index = tareas.findIndex(t => t.id === id);
+
+  if(index !== -1){
+    const tareaBorrada = tareas.splice(index, 1)[0];
+    res.status(200).json({mensaje: `Se borro exitosamente la tarea ${id}`});
+  }else{
+    res.status(404).json({mensaje: 'Tearea no encontrada'});
+  }
+})
+
+app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
